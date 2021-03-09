3bb030907fa2d2f802b667cfc8a650097a0f7f64
kaiyong.xie <kaiyong.xie@geely.com>
2021-03-09 15:39:29 +0800

    角色大小比例

--- src/engine/plugin/player.rs
+++ src/engine/plugin/player.rs
@@ -24,14 +24,14 @@ fn setup(
     asset_server: Res<AssetServer>,
 ) {
     let texture_handle = asset_server.load("textures/chars/slime-orange.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(12.0, 12.0), 4, 1);
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 24.0), 4, 1);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     commands
         .spawn(SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
             transform: Transform {
                 translation: Vec3::new(0.0, 0.0, 1.0),
-                scale: Vec3::splat(6.0),
+                scale: Vec3::splat(5.0),
                 ..Default::default()
             },
             ..Default::default()
