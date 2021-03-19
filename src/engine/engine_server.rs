6073b0106e3c0d0fe68a7c8efe63d19e5a26b8db
JLer <472102489@qq.com>
2021-03-19 17:17:44 +0800

    帧同步速率模拟

--- src/engine/engine_server.rs
+++ src/engine/engine_server.rs
@@ -1,3 +1,5 @@
+use std::{thread::sleep, time::Duration};
+
 use rapier2d::geometry::{BroadPhase, ColliderBuilder, ColliderSet, NarrowPhase, SharedShape};
 use rapier2d::na::Vector2;
 use rapier2d::pipeline::PhysicsPipeline;
@@ -7,10 +9,13 @@ use rapier2d::{
 };
 
 pub fn engine_start() {
+    use std::time::Instant;
+    let start_time = Instant::now();
+
     // 物理引擎初始化配置
     let mut pipeline = PhysicsPipeline::new();
     // 世界重力
-    let gravity = Vector2::new(0.0, -9.81);
+    let gravity = Vector2::new(0.0, -100.0);
     //
     let integration_parameters = IntegrationParameters::default();
     //
@@ -29,13 +34,29 @@ pub fn engine_start() {
 
     let mut colloder_handle = None;
     // 物理引擎主循环
-    for i in 0..10 {
+    for i in 0..60 {
         if i == 0 {
+            // 地面
+            // 刚体类型
+            let rigid_body = RigidBodyBuilder::new(BodyStatus::Static)
+                .translation(0.0, -10.0)
+                .build();
+            // 碰撞体类型
+            let collider = ColliderBuilder::new(SharedShape::cuboid(5000.0, 5.0))
+                // 摩擦
+                .friction(0.0)
+                // 是否为传感器
+                // .sensor(true)
+                .build();
+            let rb_handle = bodies.insert(rigid_body);
+            colliders.insert(collider, rb_handle, &mut bodies);
+
+            // 球
             // 刚体类型
             let rigid_body = RigidBodyBuilder::new(BodyStatus::Dynamic)
-                .translation(1.0, 5.0)
-                .rotation(0.0)
-                .position(Isometry2::new(Vector2::new(1.0, 5.0), 0.0))
+                .translation(0.0, 50.0)
+                // .rotation(0.0)
+                // .position(Isometry2::new(Vector2::new(1.0, 5.0), 0.0))
                 // 线速度
                 .linvel(0.0, 0.0)
                 // 角速度
@@ -45,13 +66,13 @@ pub fn engine_start() {
                 .can_sleep(true)
                 .build();
             // 碰撞体类型
-            let collider = ColliderBuilder::new(SharedShape::ball(0.5))
+            let collider = ColliderBuilder::new(SharedShape::ball(5.0))
                 // 密度
-                .density(1.3)
+                .density(1.0)
                 // 摩擦
-                .friction(0.8)
+                .friction(0.0)
                 // 是否为传感器
-                .sensor(true)
+                // .sensor(true)
                 .build();
             let rb_handle = bodies.insert(rigid_body);
             colloder_handle = Some(colliders.insert(collider, rb_handle, &mut bodies));
@@ -73,5 +94,9 @@ pub fn engine_start() {
             println!("刚体位置: {}", rb.position().translation);
             println!("碰撞体位置: {}", collider.position().translation);
         }
+        sleep(Duration::new(0, 14500000));
     }
+
+    let time = start_time.elapsed().as_secs_f64();
+    println!("{}", time);
 }
