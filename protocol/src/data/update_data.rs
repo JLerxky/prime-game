eafeb5bffa907529618cb58f5743030aa0fa4e00
JLer <472102489@qq.com>
2021-03-30 22:02:03 +0800

    状态同步协议初步完成

--- protocol/src/data/update_data.rs
+++ protocol/src/data/update_data.rs
@@ -3,17 +3,24 @@ use super::Data;
 // 状态同步数据
 #[derive(Debug)]
 pub struct UpdateData {
-    // 长度16b[0..15]
+    // 16b[0..15]
     pub frame: u128,
-    // 长度16b[16..31]
-    pub id: u128,
-    // 长度8b[32..39]
+    // x40b[16..]
+    pub states: Vec<RigidBodyState>,
+}
+
+#[derive(Debug)]
+// 40b
+pub struct RigidBodyState {
+    // 8b[0..7]
+    pub id: u64,
+    // 8b[8..15]
     pub translation: (f32, f32),
-    // 长度8b[40..47]
+    // 8b[16..23]
     pub rotation: (f32, f32),
-    // 线速度8b[48..55]
+    // 8b[24..31]
     pub linvel: (f32, f32),
-    // 角速度8b[56..63]
+    // 8b[32..39]
     pub angvel: (f32, f32),
 }
 
@@ -21,15 +28,17 @@ impl Data for UpdateData {
     fn data(&self) -> Vec<u8> {
         let mut route: Vec<u8> = Vec::new();
         route.append(&mut self.frame.to_ne_bytes().to_vec());
-        route.append(&mut self.id.to_ne_bytes().to_vec());
-        route.append(&mut self.translation.0.to_ne_bytes().to_vec());
-        route.append(&mut self.translation.1.to_ne_bytes().to_vec());
-        route.append(&mut self.rotation.0.to_ne_bytes().to_vec());
-        route.append(&mut self.rotation.1.to_ne_bytes().to_vec());
-        route.append(&mut self.linvel.0.to_ne_bytes().to_vec());
-        route.append(&mut self.linvel.1.to_ne_bytes().to_vec());
-        route.append(&mut self.angvel.0.to_ne_bytes().to_vec());
-        route.append(&mut self.angvel.1.to_ne_bytes().to_vec());
+        for rb in &self.states {
+            route.append(&mut rb.id.to_ne_bytes().to_vec());
+            route.append(&mut rb.translation.0.to_ne_bytes().to_vec());
+            route.append(&mut rb.translation.1.to_ne_bytes().to_vec());
+            route.append(&mut rb.rotation.0.to_ne_bytes().to_vec());
+            route.append(&mut rb.rotation.1.to_ne_bytes().to_vec());
+            route.append(&mut rb.linvel.0.to_ne_bytes().to_vec());
+            route.append(&mut rb.linvel.1.to_ne_bytes().to_vec());
+            route.append(&mut rb.angvel.0.to_ne_bytes().to_vec());
+            route.append(&mut rb.angvel.1.to_ne_bytes().to_vec());
+        }
         route
     }
 }
@@ -39,49 +48,65 @@ impl UpdateData {
         let ptr: *const u8 = data[0..15].as_ptr();
         let ptr: *const u128 = ptr as *const u128;
         let frame = unsafe { *ptr };
+        let mut states = Vec::new();
 
-        let ptr: *const u8 = data[16..31].as_ptr();
-        let ptr: *const u128 = ptr as *const u128;
-        let id = unsafe { *ptr };
+        let state_no = (data.len() - 16) / 40;
 
-        let ptr: *const u8 = data[32..35].as_ptr();
-        let ptr: *const f32 = ptr as *const f32;
-        let translation0 = unsafe { *ptr };
+        for i in 0..state_no {
+            let i = i + (i * 39) + 16;
+            let ptr: *const u8 = data[i..(i + 7)].as_ptr();
+            let ptr: *const u64 = ptr as *const u64;
+            let id = unsafe { *ptr };
 
-        let ptr: *const u8 = data[36..39].as_ptr();
-        let ptr: *const f32 = ptr as *const f32;
-        let translation1 = unsafe { *ptr };
+            let i = i + 8;
+            let ptr: *const u8 = data[i..(i + 3)].as_ptr();
+            let ptr: *const f32 = ptr as *const f32;
+            let translation_x = unsafe { *ptr };
 
-        let ptr: *const u8 = data[40..43].as_ptr();
-        let ptr: *const f32 = ptr as *const f32;
-        let rotation0 = unsafe { *ptr };
+            let i = i + 4;
+            let ptr: *const u8 = data[i..(i + 3)].as_ptr();
+            let ptr: *const f32 = ptr as *const f32;
+            let translation_y = unsafe { *ptr };
 
-        let ptr: *const u8 = data[44..47].as_ptr();
-        let ptr: *const f32 = ptr as *const f32;
-        let rotation1 = unsafe { *ptr };
+            let i = i + 4;
+            let ptr: *const u8 = data[i..(i + 3)].as_ptr();
+            let ptr: *const f32 = ptr as *const f32;
+            let rotation0 = unsafe { *ptr };
 
-        let ptr: *const u8 = data[48..51].as_ptr();
-        let ptr: *const f32 = ptr as *const f32;
-        let linvel0 = unsafe { *ptr };
+            let i = i + 4;
+            let ptr: *const u8 = data[i..(i + 3)].as_ptr();
+            let ptr: *const f32 = ptr as *const f32;
+            let rotation1 = unsafe { *ptr };
 
-        let ptr: *const u8 = data[52..55].as_ptr();
-        let ptr: *const f32 = ptr as *const f32;
-        let linvel1 = unsafe { *ptr };
+            let i = i + 4;
+            let ptr: *const u8 = data[i..(i + 3)].as_ptr();
+            let ptr: *const f32 = ptr as *const f32;
+            let linvel_x = unsafe { *ptr };
 
-        let ptr: *const u8 = data[56..59].as_ptr();
-        let ptr: *const f32 = ptr as *const f32;
-        let angvel0 = unsafe { *ptr };
+            let i = i + 4;
+            let ptr: *const u8 = data[i..(i + 3)].as_ptr();
+            let ptr: *const f32 = ptr as *const f32;
+            let linvel_y = unsafe { *ptr };
 
-        let ptr: *const u8 = data[60..63].as_ptr();
-        let ptr: *const f32 = ptr as *const f32;
-        let angvel1 = unsafe { *ptr };
-        UpdateData {
-            frame,
-            id,
-            translation: (translation0, translation1),
-            rotation: (rotation0, rotation1),
-            linvel: (linvel0, linvel1),
-            angvel: (angvel0, angvel1),
+            let i = i + 4;
+            let ptr: *const u8 = data[i..(i + 3)].as_ptr();
+            let ptr: *const f32 = ptr as *const f32;
+            let angvel0 = unsafe { *ptr };
+
+            let i = i + 4;
+            let ptr: *const u8 = data[i..(i + 3)].as_ptr();
+            let ptr: *const f32 = ptr as *const f32;
+            let angvel1 = unsafe { *ptr };
+
+            states.push(RigidBodyState {
+                id,
+                translation: (translation_x, translation_y),
+                rotation: (rotation0, rotation1),
+                linvel: (linvel_x, linvel_y),
+                angvel: (angvel0, angvel1),
+            })
         }
+
+        UpdateData { frame, states }
     }
 }
--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -48,18 +48,86 @@ impl Packet {
 fn test() {
     // let i: u128 = 340282366920938463463374607431768211455;
     // println!("{}{}", Packet::Heartbeat as u8, i / 120);
-    let packet: Packet = Packet::Game(GameRoute::Update(data::update_data::UpdateData {
-        frame: 0,
+    let mut states = Vec::new();
+    states.push(data::update_data::RigidBodyState {
         id: 1,
-        translation: (1.534563456f32, 0.132412f32),
-        rotation: (2f32, 2f32),
-        linvel: (3f32, 3f32),
-        angvel: (4f32, 4f32),
+        translation: (1., 1.),
+        rotation: (1., 1.),
+        linvel: (1., 1.),
+        angvel: (1., 1.),
+    });
+    states.push(data::update_data::RigidBodyState {
+        id: 2,
+        translation: (2., 2.),
+        rotation: (2., 2.),
+        linvel: (2., 2.),
+        angvel: (2., 2.),
+    });
+    states.push(data::update_data::RigidBodyState {
+        id: 3,
+        translation: (3., 3.),
+        rotation: (3., 3.),
+        linvel: (3., 3.),
+        angvel: (3., 3.),
+    });
+    states.push(data::update_data::RigidBodyState {
+        id: 4,
+        translation: (4., 4.),
+        rotation: (4., 4.),
+        linvel: (4., 4.),
+        angvel: (4., 4.),
+    });
+    states.push(data::update_data::RigidBodyState {
+        id: 5,
+        translation: (5., 5.),
+        rotation: (5., 5.),
+        linvel: (5., 5.),
+        angvel: (5., 5.),
+    });
+    states.push(data::update_data::RigidBodyState {
+        id: 6,
+        translation: (6., 6.),
+        rotation: (6., 6.),
+        linvel: (6., 6.),
+        angvel: (6., 6.),
+    });
+    states.push(data::update_data::RigidBodyState {
+        id: 7,
+        translation: (7., 7.),
+        rotation: (7., 7.),
+        linvel: (7., 7.),
+        angvel: (7., 7.),
+    });
+    states.push(data::update_data::RigidBodyState {
+        id: 8,
+        translation: (8., 8.),
+        rotation: (8., 8.),
+        linvel: (8., 8.),
+        angvel: (8., 8.),
+    });
+    states.push(data::update_data::RigidBodyState {
+        id: 9,
+        translation: (9., 9.),
+        rotation: (9., 9.),
+        linvel: (9., 9.),
+        angvel: (9., 9.),
+    });
+    states.push(data::update_data::RigidBodyState {
+        id: 10,
+        translation: (10., 10.),
+        rotation: (10., 10.),
+        linvel: (10., 10.),
+        angvel: (10., 10.),
+    });
+    let packet: Packet = Packet::Game(GameRoute::Update(data::update_data::UpdateData {
+        frame: 1,
+        states,
     }));
-    println!("{:?}", packet.to_bytes().len());
-    println!("{:?}", packet.to_bytes()[2..].len());
+    let packet = packet.to_bytes();
+    println!("{}", packet.len());
+    println!("{:?}", packet);
     println!(
         "{:?}",
-        data::update_data::UpdateData::from(packet.to_bytes()[2..].to_vec())
+        data::update_data::UpdateData::from(packet[2..].to_vec())
     );
 }
