86e215e0b4ecb7f05394fd59562db1c33d53a8b1
JLer <472102489@qq.com>
2021-04-02 23:45:51 +0800

    配置阿里云服务器地址

--- common/src/config.rs
+++ common/src/config.rs
@@ -1,7 +1,7 @@
 pub const SERVER_PORT: u16 = 2101;
 // 服务器地址
-pub const SERVER_ADDR: &str = "0.0.0.0:2101";
+pub const SERVER_ADDR: &str = "jler.vip:2101";
 // 帧间时间
 pub const INTER_FRAME_TIME: f64 = 1f64 / 60f64;
 // 当前玩家uid
-pub const UID: u32 = 4726;
+pub const UID: u32 = 4721;
