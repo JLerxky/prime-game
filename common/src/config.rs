cdfdaf8c2770b82fd0bd22d95fe06b94932c0e52
JLer <472102489@qq.com>
2021-04-11 01:05:14 +0800

    阿里云发布

--- common/src/config.rs
+++ common/src/config.rs
@@ -1,6 +1,6 @@
 pub const SERVER_PORT: u16 = 2101;
 // 服务器地址
-pub const SERVER_ADDR: &str = "192.168.101.198:2101";
+pub const SERVER_ADDR: &str = "0.0.0.0:2101";
 // 客户端地址
 pub const CLIENT_ADDR: &str = "0.0.0.0:0";
 // 帧间时间
