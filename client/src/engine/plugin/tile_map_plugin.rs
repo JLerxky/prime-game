45cb0ef155dcaa384cf429d7ec6cf79e36c3a4be
JLer <472102489@qq.com>
2021-05-05 00:49:56 +0800

    优化帧率

--- client/src/engine/plugin/tile_map_plugin.rs
+++ client/src/engine/plugin/tile_map_plugin.rs
@@ -83,11 +83,11 @@ fn setup(
     // 计算tile_size大小
     let tile_size = tile_map.texture_size * tile_map.chunk_size;
     // 根据窗口大小修改tile_map大小
-    let mut x = (window.width / tile_size.x as f32) as u32 + 2;
-    let mut y = (window.height / tile_size.y as f32) as u32 + 2;
-    x += ((x % 2) == 0) as u32;
-    y += ((y % 2) == 0) as u32;
-    tile_map.map_size = UVec3::new(x, y, tile_map.map_size.z);
+    // let mut x = (window.width / tile_size.x as f32) as u32 + 2;
+    // let mut y = (window.height / tile_size.y as f32) as u32 + 2;
+    // x += ((x % 2) == 0) as u32;
+    // y += ((y % 2) == 0) as u32;
+    // tile_map.map_size = UVec3::new(x, y, tile_map.map_size.z);
 
     let center_pos = tile_map.center_point.as_f32()
         * tile_map.texture_size.as_f32()
